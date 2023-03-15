d6f796b49174fe01d97244d19d1cca274dd85d4a
Aayush Gupta <aayushgup18@gmail.com>
2023-03-15 11:53:09 +0530

    routes

--- server.js
+++ server.js
@@ -3,5 +3,13 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 app.get('/',(req,res) => res.send('API Running'));
+
+
+//define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
