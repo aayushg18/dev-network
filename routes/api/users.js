5b2850ebcb84c1ad3d81ae0ab63572df66333c6e
Aayush Gupta <aayushgup18@gmail.com>
2023-03-24 16:52:36 +0530

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/Users'); 
 
 // @route GET api/users
@@ -25,9 +26,27 @@ router.post('/',[
     try {
         let user = await User.findOne({email});
         if(user){
-            res.status(400).json();
+            return res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
         }
-        res.send('User Route');
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password,salt);
+        
+        await user.save();
+
+        res.send('User Registered');
     } catch(err) {
         console.log(err.message);
         res.status(500).send('Server Error');
